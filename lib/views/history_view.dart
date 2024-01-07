3668e2748aedb56abf35d81844d9dcc77246684f
Harith A. <harith2312@gmail.com>
2024-01-08 01:43:05 +0800

    remove firebase directly

--- lib/views/history_view.dart
+++ lib/views/history_view.dart
@@ -1,5 +1,4 @@
 // import 'package:flutter/foundation.dart';
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:influenza/services/auth/auth_service.dart';
 import 'package:influenza/services/cloud/firebase_cloud_storage.dart';
