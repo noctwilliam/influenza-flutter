a146656038d1e0e2ef8e2e310226c8935076c45f
Harith A <harith2312@gmail.com>
2024-01-08 11:19:43 +0800

    forgot to stage faq_view

---
+++ lib/views/faq_view.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+import 'package:hooks_riverpod/hooks_riverpod.dart';
+
+class FaqView extends ConsumerWidget {
+  const FaqView({super.key});
+
+  @override
+  Widget build(BuildContext context, WidgetRef ref) {
+    return Container();
+  }
+}
